c7d6f58fd560cc45cd2d5fe6bce9e7a20797336f
amukherj <arindam.mukerjee@gmail.com>
2019-06-29 13:31:18 +0530

    Renamed the function `style` to `echo_style` and introduced an inline styling function `istyle`.

--- text/textstyle.sh
+++ text/textstyle.sh
@@ -1,26 +1,51 @@
 #!/bin/sh
 
+black=0
 red=1
 green=2
-amber=3
+yellow=3
 blue=4
-pink=5
-sky=6
-white=7
-orange=9
-grey=10
-ash=11
-lavender=13
-indigo=16
+magenta=5
+cyan=6
+lightgray=7
+darkgray=9
+lightred=10
+lightgreen=11
+amber=12
+lightblue=13
+pink=14
+lightcyan=15
 navy=17
 
+iblack=30
+ired=31
+igreen=32
+iyellow=33
+iblue=34
+imagenta=35
+icyan=36
+ilightgray=37
+idarkgray=90
+ilightred=91
+ilightgreen=92
+iamber=93
+ilightblue=94
+ipink=95
+ilightcyan=96
+iwhite=97
+
+ibold=1
+idim=2
+iul=4
+iblink=5
+
 text() {
-  color=${1:-${white}}
+  color=${1:-${lightgray}}
   echo "setaf ${color}"
 }
 
 bg() {
-  color=${1:-${white}}
+  color=${1:-${lightgray}}
   echo "setab ${color}"
 }
 
@@ -40,7 +65,13 @@ bold() {
   echo bold
 }
 
-style() {
+## echo_style: echo a stylized version of the passed text
+## echo the first argument after applying styles
+## passed in the additional arguments.
+## Examples:
+##   echo_style "Hello, world!" "text $green" ul blink bold
+##
+echo_style() {
   txt=${1}
   if [ -z "$txt" ]; then
     return
@@ -59,22 +90,70 @@ style() {
   echo -n ${txt}; tput sgr0
 }
 
+## istyle: inline style.
+## generate stylized version of first argument after applying styles
+## passed in the additional arguments. The result can be printed by
+## echo, printf, read -p "..." , etc.
+## On many terminals, only red, lightred, pink, magenta are properly rendered.
+## Examples:
+##   str=$(istyle "Hello, world!" lightyellow bold ul)
+##
+istyle() {
+  txt=${1}
+  if [ -z "$txt" ]; then
+    return
+  fi
+
+  shift
+  if [ $# -le 0 ]; then
+    return
+  fi
+
+  prefix=
+  for style in $@; do
+    newstyle="i${style}"
+    prefix="${prefix}\\\\e[\${${newstyle}}m"
+  done
+  suffix="\e[0m"
+  prefix=$(eval echo ${prefix})
+  echo "${prefix}${txt}${suffix}"
+}
+
 ### Examples
 shellutil_textstyle_run_example() {
-  style "Hello, there!" "text $green" ul blink
+  echo_style "Hello, there!" "text $green" ul blink
   echo ""
-  style "Hello, there!" "text $blue" ul bold
+  echo_style "Hello, there!" "text $blue" ul bold
   echo ""
-  style "Hello, there!" "text $pink" ul
+  echo_style "Hello, there!" "text $magenta" ul
   echo ""
-  style "Hello, there!" "text $sky" blink
+  echo_style "Hello, there!" "text $cyan" blink
   echo ""
-  style "Hello, there!" "text $amber" ul em
+  echo_style "Hello, there!" "text $amber" ul em
   echo ""
-  style "Hello, there!" "bg $lavender" 
+  echo_style "Hello, there!" "bg $lavender" 
   echo ""
-  style "Hello, there!" "bg $white" "text $red" bold
+  echo_style "Hello, there!" "bg $lightgray" "text $red" bold
   echo ""
-  style "Hello, there!" "text $white" "bg $blue" blink
+  echo_style "Hello, there!" "text $lightgray" "bg $blue" blink
   echo ""
+
+  echo "#### Starts here"
+  for i in {0..17}; do
+    echo_style "INFO: " "text $i"
+    echo "Data written"
+  done
+
+  echo_style "WARN: " "text $amber"
+  echo "Low disk space"
+
+  echo_style "ERROR: " "text $red"
+  echo "Permission denied"
+
+  echo "#### istyle"
+  str=$(istyle Hello red bold ul)
+  read -p "$str world!" n
+  echo $n
+  printf "$str world!\n"
 }
+
